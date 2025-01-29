a6e11ba319c124a048c5f46b569964bf624cf303
maks2001916 <mamonov.maksim.20003@gmail.com>
2025-01-29 12:37:15 +0500

    Элементы view привязаны к соответствующим объектам класса

--- app/src/main/java/com/example/mywardrobe/MainActivity.kt
+++ app/src/main/java/com/example/mywardrobe/MainActivity.kt
@@ -1,16 +1,37 @@
 package com.example.mywardrobe
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var titleTV: TextView
+    private lateinit var startBTN: Button
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
+        init()
+
+        startBTN.setOnClickListener {
+            val intent = Intent(this, MyWardrobeActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+
+    private fun init() {
+        titleTV = findViewById(R.id.titleTV)
+        startBTN = findViewById(R.id.startBTN)
     }
 }
