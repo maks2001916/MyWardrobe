4b24468c9e4b08615d4127363fe2dd22270e3b07
maks2001916 <mamonov.maksim.20003@gmail.com>
2025-01-29 18:33:43 +0500

    Добавлена конопка возврата назад в toolbar

--- app/src/main/java/com/example/mywardrobe/ClothingInfoActivity.kt
+++ app/src/main/java/com/example/mywardrobe/ClothingInfoActivity.kt
@@ -2,6 +2,8 @@ package com.example.mywardrobe
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.TextView
 import androidx.appcompat.widget.Toolbar
 import androidx.activity.enableEdgeToEdge
@@ -31,5 +33,33 @@ class ClothingInfoActivity : AppCompatActivity() {
         titleTV = findViewById(R.id.titleInfoTV)
         descriptionTV = findViewById(R.id.descriptionInfoTV)
         setSupportActionBar(toolbarTB)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        toolbarTB.setNavigationOnClickListener { onBackPressed() }
+        var clothing: Clothing? = null
+        if (intent.hasExtra("image") &&
+            intent.hasExtra("title") &&
+            intent.hasExtra("description")) {
+            clothing = Clothing(
+                intent.getStringExtra("image")!!,
+                intent.getStringExtra("title")!!,
+                intent.getStringExtra("description")!!)
+        }
+        if (clothing != null) {
+            imageCIV.setImageResource(clothing.image.toInt())
+            titleTV.text = clothing.title
+            descriptionTV.text = clothing.description
+        }
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.wardrobe_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {R.id.exit -> finishAffinity() }
+        return super.onOptionsItemSelected(item)
+    }
+
+
 }
